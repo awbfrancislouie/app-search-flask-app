58ed218fe59da3d4e607d3a461f378661a97ea36
aravindputrevu <aravind.putrevu@elastic.co>
2020-05-28 15:26:18 +0530

    Adding App Search
    1. Add your App Search base endpoint, api_key, engine_name in "config.json"
    2. Added a method to index data from file "data.json".
    3. About.html shows data returned from indexer.

--- main.py
+++ main.py
@@ -1,10 +1,30 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request     
+from elastic_app_search import Client
+import json
 
 app = Flask(__name__)
+
+client = Client(
+    base_endpoint=config['appsearch']['base_endpoint'],
+    api_key=config['appsearch']['api_key'],
+    use_https=True)
+
+engine_name = config['appsearch']['engine_name']
 
 @app.route("/")
 def home():
     return render_template("home.html")
+     
+@app.route("/index")
+def index():
+    # Opening JSON file 
+    f = open('data.json',) 
+
+    # returns JSON object as  
+    # a dictionary 
+    documents = json.load(f)
+    data = client.index_documents(engine_name, documents)
+    return render_template("about.html" , data=data)
     
 if __name__ == "__main__":
     app.run(debug=False)
